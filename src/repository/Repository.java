6415c245ce90d5c87e5446003ec81e719f5f22bc
hoanggiabao1804 <hoanggiabao1804@gmail.com>
2026-05-19 00:24:06 +0700

    Repository now will have only 2 behaviors: importData and exportData.

--- src/repository/Repository.java
+++ src/repository/Repository.java
@@ -1,21 +1,8 @@
 package repository;
 
-import java.util.List;
-
 public interface Repository {
 
     public void importData(String path);
 
     public void exportData(String path);
-
-    public List<Object> findAll();
-
-    public Object findById(String id);
-
-    public boolean existsById(String id);
-
-    public Object save(Object save);
-
-    public void deleteById(String id);
-
 }
