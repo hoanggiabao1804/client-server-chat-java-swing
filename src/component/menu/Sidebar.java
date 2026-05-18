345857407367c891e3f7b60a53e660accaeb0748
hoanggiabao1804 <hoanggiabao1804@gmail.com>
2026-05-19 00:25:40 +0700

    Sidebar will be loaded after user login.

--- src/component/menu/Sidebar.java
+++ src/component/menu/Sidebar.java
@@ -1,5 +1,6 @@
 package component.menu;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Container;
@@ -13,12 +14,14 @@ import java.awt.GridBagLayout;
 import java.awt.RenderingHints;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -30,15 +33,21 @@ import javax.swing.border.EmptyBorder;
 
 import component.AppContext;
 import component.AppFrame;
+import domain.Dialog;
+import domain.Message;
 import domain.User;
-import repository.UserRepository;
+import repository.DialogRepository;
+import util.Authentication;
 
 public class Sidebar implements AppContext {
 
     private final Container parent;
     private final Dimension size;
-    private final Map<String, User> userPool = new HashMap<>();
+    private final Map<String, Dialog> storage = new HashMap<>();
     private final Locale locale = Locale.of("vi");
+    private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm dd 'Tháng' MM, yyyy");
+
+    private User userLogin;
 
     // Font & color
     private final Font headerFont;
@@ -61,7 +70,7 @@ public class Sidebar implements AppContext {
     // Body section
     private JScrollPane scrollPane;
     private List<JPanel> dialogTabList;
-    private User selectedUser;
+    private Dialog selectedDialog;
     private JPanel selectedDialogTab;
 
     public Sidebar(Container parent, Dimension size) {
@@ -73,16 +82,6 @@ public class Sidebar implements AppContext {
         this.tabFont = new Font("Consolas", Font.BOLD, 20);
         this.textFont = new Font("Consolas", Font.PLAIN, 15);
         this.sidebarColor = new Color(0, 129, 138);
-
-        List<User> userList = UserRepository.getInstance().findAll().stream().map(item -> (User) item)
-                .collect(Collectors.toList());
-
-        userList.forEach(item -> {
-            this.userPool.put(item.getId().toString(), item);
-        });
-    }
-
-    public void init() {
 
         // Container initialization
         sidebarContainer = new JPanel();
@@ -98,10 +97,11 @@ public class Sidebar implements AppContext {
 
         // Body initialization
         scrollPane = new JScrollPane(bodyContainer);
-        dialogTabList = this.userPool.values().stream().map(item -> {
-            return createDialogTabPanel(item);
-        }).collect(Collectors.toList());
-        selectedUser = null;
+        dialogTabList = null;
+        // dialogTabList = this.storage.values().stream().map(item -> {
+        // return createDialogTabPanel(item);
+        // }).collect(Collectors.toList());
+        selectedDialog = null;
         selectedDialogTab = null;
 
         GridBagConstraints gbc = new GridBagConstraints();
@@ -140,8 +140,6 @@ public class Sidebar implements AppContext {
             String keyword = searchTextField.getText().strip();
 
             this.search(keyword.toLowerCase(locale));
-            this.bodyContainer.revalidate();
-            this.bodyContainer.repaint();
         });
 
         searchButton.setIcon(searchIcon);
@@ -155,50 +153,58 @@ public class Sidebar implements AppContext {
         });
 
         // Body section
-        bodyContainer.setLayout(new FlowLayout(FlowLayout.LEADING));
-        bodyContainer.setPreferredSize(new Dimension(size.width - 20, dialogTabList.size() * 100));
+        bodyContainer.setLayout(new BoxLayout(bodyContainer, BoxLayout.Y_AXIS));
+        // bodyContainer.setPreferredSize(new Dimension(size.width - 20,
+        // dialogTabList.size() * 100));
         bodyContainer.setBackground(Color.white);
 
-        dialogTabList.forEach(item -> bodyContainer.add(item));
+        // dialogTabList.forEach(item -> bodyContainer.add(item));
 
         scrollPane.getVerticalScrollBar().setUnitIncrement(16);
         scrollPane.setLayout(new ScrollPaneLayout());
         scrollPane.setPreferredSize(new Dimension(size.width - 10, size.height - 130));
         scrollPane.setBorder(new EmptyBorder(5, 0, 5, 0));
         scrollPane.setBackground(Color.lightGray);
+
     }
 
-    private JPanel createDialogTabPanel(User user) {
+    private JPanel createDialogTabPanel(Dialog dialog) {
         JPanel rootPanel = new JPanel();
+        JPanel avatarPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 0));
         RoundButton avatarButton = new RoundButton();
         ImageIcon avatarIcon = new ImageIcon("assets/user-round.png");
         JPanel contentPanel = new JPanel();
         JLabel nameLabel = new JLabel();
         JLabel lastMessageLabel = new JLabel();
 
+        int tabHeight = 80;
+        int tabWidth = this.size.width - 20;
+
         // Root section
-        rootPanel.setLayout(new FlowLayout());
+        rootPanel.setLayout(new BorderLayout(10, 0));
+        rootPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
         rootPanel.setBackground(Color.white);
-        rootPanel.add(avatarButton);
-        rootPanel.add(contentPanel);
+        rootPanel.setPreferredSize(new Dimension(tabWidth, tabHeight));
+        rootPanel.setMaximumSize(new Dimension(Integer.MAX_VALUE, tabHeight));
+        rootPanel.setMinimumSize(new Dimension(tabWidth, tabHeight));
         rootPanel.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent e) {
-                if (selectedUser != null) {
-                    if (selectedUser != user) {
+                if (selectedDialog != null) {
+                    if (selectedDialog != dialog) {
                         selectedDialogTab.setBackground(Color.white);
                         selectedDialogTab = rootPanel;
                         selectedDialogTab.setBackground(Color.blue);
 
-                        selectedUser = user;
+                        selectedDialog = dialog;
                     }
                 } else {
-                    selectedUser = user;
+                    selectedDialog = dialog;
                     selectedDialogTab = rootPanel;
                     selectedDialogTab.setBackground(Color.blue);
                 }
 
                 MainMenu mainMenu = (MainMenu) AppFrame.getInstance().getContextPools().getContext("mainMenu");
-                mainMenu.loadDialogDetail(user);
+                mainMenu.loadDialogDetail(dialog);
             }
 
             public void mouseEntered(MouseEvent e) {
@@ -214,59 +220,75 @@ public class Sidebar implements AppContext {
             }
         });
 
+        avatarPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
+        avatarPanel.setOpaque(false);
+        avatarPanel.setPreferredSize(new Dimension(60, 50));
+
         // Content section
         avatarButton.setIcon(avatarIcon);
         avatarButton.setPreferredSize(new Dimension(50, 50));
+        avatarButton.setMinimumSize(new Dimension(50, 50));
+        avatarButton.setMaximumSize(new Dimension(50, 50));
         avatarButton.setFocusable(false);
 
-        contentPanel.setLayout(new FlowLayout());
-        contentPanel.setPreferredSize(new Dimension(this.size.width - 80, 100));
-        contentPanel.setBackground(Color.white);
+        avatarPanel.add(avatarButton);
+
+        // contentPanel.setLayout(new FlowLayout());
+        // contentPanel.setPreferredSize(new Dimension(this.size.width - 80, 100));
+        contentPanel.setOpaque(false);
+        contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
+
+        // Dimension labelSize = new Dimension(this.size.width - 80, 30);
+
+        // nameLabel.setPreferredSize(labelSize);
+        nameLabel.setText(dialog.getName());
+        nameLabel.setFont(this.tabFont);
+
+        // lastMessageLabel.setPreferredSize(labelSize);
+        Message lastMsg = dialog.getMessages().isEmpty() ? null : dialog.getMessages().getLast();
+
+        lastMessageLabel.setText(lastMsg != null
+                ? lastMsg.getSenderId().equals(userLogin.getId().toString()) ? "Bạn: " + lastMsg.getContent()
+                        : lastMsg.getContent()
+                : "");
+        lastMessageLabel.setFont(this.textFont);
+
         contentPanel.add(nameLabel);
         contentPanel.add(lastMessageLabel);
 
-        Dimension labelSize = new Dimension(this.size.width - 80, 30);
-
-        nameLabel.setPreferredSize(labelSize);
-        nameLabel.setText(user.getName());
-        nameLabel.setFont(this.tabFont);
-
-        lastMessageLabel.setPreferredSize(labelSize);
-        lastMessageLabel.setText("Đây là tin nhắn cuối cùng trong đoạn chat");
-        lastMessageLabel.setFont(this.textFont);
+        rootPanel.add(avatarPanel, BorderLayout.WEST);
+        rootPanel.add(contentPanel, BorderLayout.CENTER);
 
         return rootPanel;
     }
 
-    public void revalidateData(User user) {
-        this.selectedUser = user;
+    public void revalidateData(Dialog dialog) {
+        this.selectedDialog = dialog;
 
-        dialogTabList = this.userPool.values().stream().map(item -> {
+        dialogTabList = this.storage.values().stream().map(item -> {
             return createDialogTabPanel(item);
         }).collect(Collectors.toList());
 
         bodyContainer.removeAll();
-        bodyContainer.setPreferredSize(new Dimension(size.width - 20, dialogTabList.size() * 100));
         dialogTabList.forEach(item -> bodyContainer.add(item));
     }
 
-    public void setUserPool(List<User> userPool) {
-        this.userPool.clear();
-        userPool.forEach(item -> {
-            this.userPool.put(item.getId().toString(), item);
+    public void setDialogPool(List<Dialog> dialogPool) {
+        this.storage.clear();
+        dialogPool.forEach(item -> {
+            this.storage.put(item.getId(), item);
         });
 
-        dialogTabList = this.userPool.values().stream().map(item -> {
+        dialogTabList = this.storage.values().stream().map(item -> {
             return createDialogTabPanel(item);
         }).collect(Collectors.toList());
 
         bodyContainer.removeAll();
-        bodyContainer.setPreferredSize(new Dimension(size.width - 20, dialogTabList.size() * 100));
         dialogTabList.forEach(item -> bodyContainer.add(item));
     }
 
     private void search(String keyword) {
-        List<User> searchedUsers = this.userPool.values().stream().filter(item -> {
+        List<Dialog> searchedDialogs = this.storage.values().stream().filter(item -> {
             if (keyword.isBlank()) {
                 return true;
             }
@@ -278,19 +300,38 @@ public class Sidebar implements AppContext {
             return false;
         }).collect(Collectors.toList());
 
-        dialogTabList = searchedUsers.stream().map(item -> {
+        dialogTabList = searchedDialogs.stream().map(item -> {
             return createDialogTabPanel(item);
         }).collect(Collectors.toList());
 
-        if (dialogTabList.size() > 0) {
-            for (int i = 0; i < 10; ++i) {
-                dialogTabList.add(createDialogTabPanel(searchedUsers.get(0)));
-            }
-        }
+        // if (dialogTabList.size() > 0) {
+        // for (int i = 0; i < 10; ++i) {
+        // dialogTabList.add(createDialogTabPanel(searchedDialogs.get(0)));
+        // }
+        // }
 
         bodyContainer.removeAll();
-        bodyContainer.setPreferredSize(new Dimension(size.width - 20, dialogTabList.size() * 115));
         dialogTabList.forEach(item -> bodyContainer.add(item));
+        this.bodyContainer.revalidate();
+        this.bodyContainer.repaint();
+    }
+
+    public void loadUser() {
+        userLogin = Authentication.getInstance().getUserLogin();
+
+        List<Dialog> dialogList = DialogRepository.getInstance().findByUserId(userLogin.getId().toString());
+
+        dialogList.forEach(item -> {
+            this.storage.put(item.getId(), item);
+        });
+
+        dialogTabList = this.storage.values().stream().map(item -> {
+            return createDialogTabPanel(item);
+        }).collect(Collectors.toList());
+
+        dialogTabList.forEach(item -> bodyContainer.add(item));
+        this.bodyContainer.revalidate();
+        this.bodyContainer.repaint();
     }
 
     @Override
@@ -350,11 +391,18 @@ class RoundButton extends JButton {
                 RenderingHints.KEY_ANTIALIASING,
                 RenderingHints.VALUE_ANTIALIAS_ON);
 
-        // Background
-        g2.setColor(this.getBackground());
+        // // Background
+        // g2.setColor(this.getBackground());
 
-        // Round corners
-        g2.fillOval(0, 0, getWidth(), getHeight());
+        // // Round corners
+        // g2.fillOval(0, 0, getWidth(), getHeight());
+
+        int size = Math.min(getWidth(), getHeight());
+        int x = (getWidth() - size) / 2;
+        int y = (getHeight() - size) / 2;
+
+        g2.setColor(getBackground());
+        g2.fillOval(x, y, size, size);
 
         super.paintComponent(g2);
         g2.dispose();
@@ -364,8 +412,15 @@ class RoundButton extends JButton {
     protected void paintBorder(Graphics g) {
         Graphics2D g2 = (Graphics2D) g.create();
 
+        // g2.setColor(Color.BLACK);
+        // g2.drawOval(0, 0, getWidth() - 1, getHeight() - 1);
+
+        int size = Math.min(getWidth(), getHeight());
+        int x = (getWidth() - size) / 2;
+        int y = (getHeight() - size) / 2;
+
         g2.setColor(Color.BLACK);
-        g2.drawOval(0, 0, getWidth() - 1, getHeight() - 1);
+        g2.drawOval(x, y, size - 1, size - 1);
 
         g2.dispose();
     }
