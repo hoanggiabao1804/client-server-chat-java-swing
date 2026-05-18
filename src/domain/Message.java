5e4da096b7cc20bc49d65c115718c0408377a1a9
hoanggiabao1804 <hoanggiabao1804@gmail.com>
2026-05-19 00:16:57 +0700

    Add message interface to define the message's behavior.

---
+++ src/domain/Message.java
@@ -0,0 +1,29 @@
+package domain;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = TextMessage.class, name = "text"),
+        @JsonSubTypes.Type(value = FileMessage.class, name = "file")
+})
+public interface Message {
+    String getType();
+
+    String getId();
+
+    String getDialogId();
+
+    String getContent();
+
+    String getSenderId();
+
+    String getReceiverId();
+
+    LocalDateTime getTimestamp();
+
+    String getTag();
+}
